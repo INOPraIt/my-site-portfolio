9f76d71474e4f21938fc0d1346cd04cb60069cf9
DEFALT <inoprait@yahoo.com>
2023-01-14 18:16:56 +0300

    first commit

---
+++ components/grid-item.js
@@ -0,0 +1,65 @@
+import NextLink from 'next/link'
+import Image from 'next/image'
+import { Box, Text, LinkBox, LinkOverlay } from '@chakra-ui/react'
+import { Global } from '@emotion/react'
+import YouTube from 'react-youtube'
+
+export const GridItem = ({ children, href, title, thumbnail }) => (
+  <Box w="100%" textAlign="center">
+    <LinkBox cursor="pointer">
+      <Image
+        src={thumbnail}
+        alt={title}
+        className="grid-item-thumbnail"
+        placeholder="blur"
+        loading="lazy"
+      />
+      <LinkOverlay href={href} target="_blank">
+        <Text mt={2}>{title}</Text>
+      </LinkOverlay>
+      <Text fontSize={14}>{children}</Text>
+    </LinkBox>
+  </Box>
+)
+
+export const PlayerGridItem = ({ children, videoId, opts }) => (
+  <Box w={'100%'} textAlign="center">
+    <YouTube
+      opts={opts}
+      videoId={videoId}
+      onReady={event => event.target.pauseVideo()}
+    />
+    <Text fontSize={14}>{children}</Text>
+  </Box>
+)
+
+export const WorkGridItem = ({ children, id, title, thumbnail }) => (
+  <Box w="100%" textAlign="center">
+    <NextLink href={`/works/${id}`} passHref scroll={false}>
+      <LinkBox cursor="pointer">
+        <Image
+          src={thumbnail}
+          alt={title}
+          className="grid-item-thumbnail"
+          placeholder="blur"
+        />
+        <LinkOverlay href={`/works/${id}`}>
+          <Text mt={2} fontSize={20}>
+            {title}
+          </Text>
+        </LinkOverlay>
+        <Text fontSize={14}>{children}</Text>
+      </LinkBox>
+    </NextLink>
+  </Box>
+)
+
+export const GridItemStyle = () => (
+  <Global
+    styles={`
+      .grid-item-thumbnail {
+        border-radius: 12px;
+      }
+    `}
+  />
+)
